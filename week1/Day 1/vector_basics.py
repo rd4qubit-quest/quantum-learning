a802689f492519a36fe009c0e379ea853e064f4a
Rohit Dangar <rd4qubit-quest>
2026-02-06 14:03:48 +0530

    Day 1: Python setup and vector basics

---
+++ week1/Day 1/vector_basics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+v = np.array([1, 0])
+w = np.array([0, 1])
+
+print("Vector v:", v)
+print("Vector w:", w)
+
+print("Dot product:", np.dot(v, w))
+print("Magnitude of v:", np.linalg.norm(v))
